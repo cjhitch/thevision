07637895808a63d26aa1c2413b6ec8ed2ac29568
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-03-27 16:08:38 -0700

    header is now mobile responsive and using full screen modal on mobile

--- reactjs/components/Header/Header.jsx
+++ reactjs/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Navbar, Nav, Button, Form, FormControl, Modal } from 'react-bootstrap';
 import styles from './Header.module.scss';
 
@@ -8,13 +9,23 @@ const Header = ({ scroll }) => {
 	return (
 		<header className={`${styles.Header} ${scroll ? styles.sticky : ''}`}>
 			<Navbar className={styles.nav}>
-				<Navbar.Brand href="#home">
-					<Image src="/logo-dark.svg" width={190} height={65} />
-				</Navbar.Brand>
+				<Link href="/">
+					<a>
+						<Image
+							src="/logo-dark.svg"
+							width={scroll ? 120 : 190}
+							height={scroll ? 45 : 65}
+						/>
+					</a>
+				</Link>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Nav className="mx-auto d-none d-lg-flex">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#link">Link</Nav.Link>
+					<Link href="/">
+						<a className={`${styles.link} h5`}>Home</a>
+					</Link>
+					<Link href="/link">
+						<a className={`${styles.link} h5`}>Link</a>
+					</Link>
 				</Nav>
 				<div className="d-none d-lg-flex">
 					<Form inline>
@@ -27,31 +38,31 @@ const Header = ({ scroll }) => {
 							className={`${styles.form} mr-sm-2`}
 						/>
 					</Form>
-					<Button variant="secondary">Let&#39;s Begin</Button>
+					<Button className={styles.button} variant="secondary">
+						Let&#39;s Begin
+					</Button>
 				</div>
+				<Button
+					className="ml-auto d-lg-none"
+					variant="outline-dark-5"
+					onClick={() => setShow(true)}
+				>
+					<i aria-hidden className="fal fa-bars px-2 fa-lg" />
+				</Button>
 			</Navbar>
 			<Modal
 				show={show}
 				onHide={() => setShow(false)}
-				dialogClassName="modal-100w"
-				aria-labelledby="example-custom-modal-styling-title"
+				dialogClassName={styles.modal}
 			>
-				<Modal.Header closeButton>
-					<Modal.Title id="example-custom-modal-styling-title">
-						Custom Modal Styling
-					</Modal.Title>
-				</Modal.Header>
+				<Modal.Header closeButton />
 				<Modal.Body>
-					<p>
-						Ipsum molestiae natus adipisci modi eligendi? Debitis
-						amet quae unde commodi aspernatur enim, consectetur.
-						Cumque deleniti temporibus ipsam atque a dolores
-						quisquam quisquam adipisci possimus laboriosam.
-						Quibusdam facilis doloribus debitis! Sit quasi quod
-						accusamus eos quod. Ab quos consequuntur eaque quo rem!
-						Mollitia reiciendis porro quo magni incidunt dolore amet
-						atque facilis ipsum deleniti rem!
-					</p>
+					<Link href="/">
+						<a className={`${styles.link} h3`}>Home</a>
+					</Link>
+					<Link href="/link">
+						<a className={`${styles.link} h3`}>Link</a>
+					</Link>
 				</Modal.Body>
 			</Modal>
 		</header>
