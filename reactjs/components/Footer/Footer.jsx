9f2107abb912f0f42e0d209350780c9588e07534
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-03-26 21:26:42 -0700

    added initial footer component and sass

---
+++ reactjs/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+	return (
+		<footer className={styles.Footer}>
+			<h1>Footer</h1>
+		</footer>
+	);
+};
+
+export default Footer;
