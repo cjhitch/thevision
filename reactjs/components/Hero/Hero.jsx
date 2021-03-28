f5acebd307aecc1bbf8d525a619e410c0f43ee26
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-03-27 17:57:47 -0700

    added in carousel with default info - set up main grid and carousel grid

--- reactjs/components/Hero/Hero.jsx
+++ reactjs/components/Hero/Hero.jsx
@@ -1,10 +1,45 @@
-import './Hero.module.scss';
+import { Carousel } from 'react-bootstrap';
+import styles from './Hero.module.scss';
 
 const Hero = () => {
 	return (
-		<div className="Hero">
-			<h1>Hero</h1>
-		</div>
+		<section className={styles.Hero}>
+			<div className={styles.overlay} />
+			<Carousel
+				interval={5000}
+				className={styles.Carousel}
+				nextIcon={<i className="fal fa-chevron-circle-right fa-2x" />}
+				prevIcon={<i className="fal fa-chevron-circle-left fa-2x" />}
+			>
+				<Carousel.Item>
+					<h1 className="display-1 text-light-4">
+						First slide label
+					</h1>
+					<p className="text-light-0">
+						Nulla vitae elit libero, a pharetra augue mollis
+						interdum.
+					</p>
+				</Carousel.Item>
+				<Carousel.Item>
+					<h1 className="display-1 text-light-4">
+						First slide label
+					</h1>
+					<p className="text-light-0">
+						Nulla vitae elit libero, a pharetra augue mollis
+						interdum.
+					</p>
+				</Carousel.Item>
+				<Carousel.Item>
+					<h1 className="display-1 text-light-4">
+						First slide label
+					</h1>
+					<p className="text-light-0">
+						Nulla vitae elit libero, a pharetra augue mollis
+						interdum.
+					</p>
+				</Carousel.Item>
+			</Carousel>
+		</section>
 	);
 };
 
