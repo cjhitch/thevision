1c98329d234cf466806dd5c5bf8ba9065ab6058c
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-03-28 13:19:52 -0700

    added alignment to title - added new about us section

--- reactjs/components/Title/Title.jsx
+++ reactjs/components/Title/Title.jsx
@@ -2,43 +2,66 @@ import PropTypes from 'prop-types';
 import styles from './Title.module.scss';
 
 const Title = ({
-	pre,
-	preClass,
-	title,
-	titleClass,
+	alignment,
+	className,
 	post,
 	postClass,
+	pre,
+	preClass,
 	Tag,
-	className,
+	title,
+	titleClass,
 }) => {
 	return (
 		<header className={`${styles.Title} ${className || ''}`}>
-			<Tag className={`${styles.title} ${titleClass || ''}`}>{title}</Tag>
+			<Tag
+				className={`${styles.title} ${titleClass || ''} ${
+					styles[alignment]
+				}`}
+			>
+				{title}
+			</Tag>
 			{pre && (
-				<h3 className={`${styles.pre} ${preClass || ''} h6`}>{pre}</h3>
+				<h3
+					className={`${styles.pre} ${preClass || ''} ${
+						styles[alignment]
+					} h6`}
+				>
+					{pre}
+				</h3>
 			)}
-			{post && <p className={`${styles.p} ${postClass || ''}`}>{post}</p>}
+			{post && (
+				<p
+					className={`${styles.p} ${postClass || ''} ${
+						styles[alignment]
+					}`}
+				>
+					{post}
+				</p>
+			)}
 		</header>
 	);
 };
 
 Title.propTypes = {
-	pre: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-	preClass: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-	title: PropTypes.string.isRequired,
-	titleClass: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+	alignment: PropTypes.string,
 	post: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 	postClass: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+	pre: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+	preClass: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 	Tag: PropTypes.string,
+	title: PropTypes.string.isRequired,
+	titleClass: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 };
 
 Title.defaultProps = {
-	pre: false,
-	preClass: false,
-	titleClass: false,
+	alignment: 'center',
 	post: false,
 	postClass: false,
+	pre: false,
+	preClass: false,
 	Tag: 'h2',
+	titleClass: false,
 };
 
 export default Title;
--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
+import { Container, Row, Col } from 'react-bootstrap';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Section from '../components/Section';
@@ -84,7 +85,7 @@ export default function Home() {
 								lg={3}
 								front={
 									<>
-										<i class="fad fa-mobile-android fa-4x mb-3"></i>
+										<i className="fad fa-mobile-android fa-4x mb-3"></i>
 										<h4>Mobile App</h4>
 									</>
 								}
@@ -105,7 +106,7 @@ export default function Home() {
 								lg={3}
 								front={
 									<>
-										<i class="fad fa-bags-shopping fa-4x mb-3"></i>
+										<i className="fad fa-bags-shopping fa-4x mb-3"></i>
 										<h4>Ecommerce</h4>
 									</>
 								}
@@ -128,7 +129,7 @@ export default function Home() {
 								lg={3}
 								front={
 									<>
-										<i class="fad fa-briefcase fa-4x mb-3"></i>
+										<i className="fad fa-briefcase fa-4x mb-3"></i>
 										<h4>Business Consultant</h4>
 									</>
 								}
@@ -151,7 +152,7 @@ export default function Home() {
 								lg={3}
 								front={
 									<>
-										<i class="fad fa-mail-bulk fa-4x mb-3"></i>
+										<i className="fad fa-mail-bulk fa-4x mb-3"></i>
 										<h4>Digital Marketing Service</h4>
 									</>
 								}
@@ -174,7 +175,7 @@ export default function Home() {
 								lg={3}
 								front={
 									<>
-										<i class="fad fa-edit fa-4x mb-3"></i>
+										<i className="fad fa-edit fa-4x mb-3"></i>
 										<h4>Web Design</h4>
 									</>
 								}
@@ -193,6 +194,65 @@ export default function Home() {
 									</p>
 								}
 							/>
+						</Row>
+					</Container>
+				</Section>
+				<Section fluid>
+					<Container>
+						<Row>
+							<Col sm={12} lg={6}>
+								<Image
+									src="/deco1.png"
+									layout="fill"
+									alt="technology in the palm of your hand and design boards"
+								/>
+							</Col>
+							<Col sm={12} lg={{ span: 6, order: 'first' }}>
+								<Title
+									alignment="left"
+									title="About Us - All Our Weird Quirks and Cool Stuff Too!"
+									titleClass="text-secondary"
+									pre="Welcome to Visionary Creatives"
+									preClass="text-dark-4"
+									post={
+										<>
+											<i
+												className={`fad fa-quote-left fa-sm mr-2 text-primary`}
+											></i>
+											<span>
+												Christopher started the company
+												in 2011 after beginning a
+												cleaning company. Finding a
+												website was necessary he worked
+												on learning that and quickly
+												fell in love. Years later, he
+												has helped hundreds of business
+												owners establish their online
+												presence and voice.
+											</span>
+											<br />
+											<br />
+											<span>
+												With over a decade of experience
+												in the web industry we have
+												worked on hundreds of websites.
+												Our expertise encompasses the
+												full gammit. With many
+												applications and websites
+												including architected APIs and
+												databases we have something for
+												everyone. Utilizing modern
+												technologies such as React and
+												Vue, whether you need ecommerce,
+												application development, web
+												design, or a new website we've
+												got you covered.
+											</span>
+										</>
+									}
+									postClass="text-dark-4"
+								/>
+							</Col>
 						</Row>
 					</Container>
 				</Section>
