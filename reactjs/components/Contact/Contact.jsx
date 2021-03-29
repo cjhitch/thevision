62e72b0245b43c078a0f6143caf138e1e988f856
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2021-03-28 22:16:25 -0700

    created new files for the contact form

---
+++ reactjs/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import Section from '../Section';
+import Title from '../Title';
+import styles from './Contact.module.scss';
+
+const Contact = () => {
+	return (
+		<Section>
+			<Title />
+		</Section>
+	);
+};
+
+export default Contact;
